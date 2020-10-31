79e51e73bd21477ea71ce97c5d87d434e8c287d6
xiahualiu <49677912+xiahualiu@users.noreply.github.com>
2020-10-31 18:02:57 -0400

    Add `--recursive` arg to git clone command.

--- install_plugs.sh
+++ install_plugs.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.vim/pack/plugs
 
 if [ ! -d ~/.vim/pack/plugs/start ]; then
 	echo "Plugins direcory does NOT exist! Creating and Copying Plugins to ~/.vim/pack/plugs/start"
-	git clone https://github.com/xiahualiu/vim-plugs.git ~/.vim/pack/plugs/start
+	git clone --recursive https://github.com/xiahualiu/vim-plugs.git ~/.vim/pack/plugs/start
 else
 	echo "Plugins directory found! Moving on to copying .vimrc"
 fi
