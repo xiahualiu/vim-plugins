1a990411dd72940d4db6691a4b65190e6aa5e228
Your Name <you@example.com>
2020-12-03 20:06:04 +0000

    Update install script.

--- install_plugs.sh
+++ install_plugs.sh
@@ -4,9 +4,9 @@ mkdir -p ~/.vim/pack/plugs
 
 if [ ! -d ~/.vim/pack/plugs/start ]; then
 	echo "Plugins direcory does NOT exist! Creating and Copying Plugins to ~/.vim/pack/plugs/start"
-	git clone --recursive https://github.com/xiahualiu/vim-plugs.git ~/.vim/pack/plugs/start
 else
-	echo "Plugins directory found! Moving on to copying .vimrc"
+	echo "Plugins directory found! Now updating the plugins!"
+	rm -rf ~/.vim/pack/plugs/start 
 fi
-
+git clone --recursive https://github.com/xiahualiu/vim-plugs.git ~/.vim/pack/plugs/start
 cp .vimrc ~/.vimrc
